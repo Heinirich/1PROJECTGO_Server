85259a34dae695474e8dc0123ea907c67d69c2c1
Heinrich <smithheinrich254@gmail.com>
2023-02-05 08:31:10 +0300

    1_GO_SERVER

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+func main()  {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/",fileServer)
+	http.HandleFunc("/form",formHandler)
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Println("http://127.0.0.1:8080")
+
+	if err := http.ListenAndServe(":8080",nil);err != nil {
+		log.Fatal("ListenAndServe error",err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.URL.Path)
+
+	if r.URL.Path != "/hello" {
+		http.Error(w,"404 Not Found",http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w,"406 Not Acceptable",http.StatusNotAcceptable)   
+	}
+	fmt.Fprint(w,"Hello, world!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request){
+
+	if err := r.ParseForm(); err != nil{
+		fmt.Fprintf(w,"Parser Error %v",err)
+		log.Fatal("ParseForm",err)
+	}
+	name := r.Form["name"][0]
+	address := r.Form["address"][0]
+	fmt.Fprintf(w,"Name: %v",name)
+	fmt.Fprintf(w,"Address: %v",address)
+
+	fmt.Fprintf(w,"Address: %v, Name : %v ",address,name)
+}
